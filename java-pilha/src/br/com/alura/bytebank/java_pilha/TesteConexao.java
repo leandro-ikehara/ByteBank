49e5f24f4685ffcc75c4344b279b457d2ba39bb7
Leandro Ikehara <leandro.ikehara@gmail.com>
2022-01-10 01:04:29 -0300

    Inserção do bloco 'finally' na execução do try/catch

--- java-pilha/src/br/com/alura/bytebank/java_pilha/TesteConexao.java
+++ java-pilha/src/br/com/alura/bytebank/java_pilha/TesteConexao.java
@@ -7,14 +7,21 @@ public class TesteConexao
 {
     public static void main(String[] args)
     {
+        Conexao conexao = null;
         try {
-            Conexao conexao = new Conexao();
+//            Conexao conexao = new Conexao();
+            conexao = new Conexao();
             conexao.leDados();
-            conexao.fecha();
+//            conexao.fecha();
         }
         catch (IllegalStateException ex)
         {
             System.out.println("Erro de conexão");
+//            conexao.fecha();
+        }
+        finally // o bloco finally é sempre executado mesmo que a conexão se feche
+        {
+            conexao.fecha();
         }
     }
 }
